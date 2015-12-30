b7df1c6192eef03146e37452c015decd0239a7bb
JackWong <me@iluckly.com>
2015-12-30 15:18:38 +0800

    first version to Developer

--- MyItem/AppDelegate.m
+++ MyItem/AppDelegate.m
@@ -17,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+//#######
+    //15171717117
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     LDYViewController *tabbarController = [[LDYViewController alloc] init];
